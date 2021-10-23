23a9b35506ee4c9ccaa2284211e47c1ac8205ab7
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-23 23:46:02 +0300

    creates a save creds in class credentials

--- credentials.py
+++ credentials.py
@@ -3,9 +3,14 @@ class Creds:
     Class that generates new 
     instances of the different social  accounts
     '''
-user_accounts=[]
-def __init__ (self,account_name,account_username,account_password):
+    user_accounts=[]
+    def __init__ (self,account_name,account_username,account_password):     
         self.account_name = account_name
         self.account_username = account_username
         self.account_password = account_password
-       
+
+    def save_creds(self):
+        '''
+        save_user method saves user objects into user_list
+    '''
+        Creds.user_accounts.append(self)
