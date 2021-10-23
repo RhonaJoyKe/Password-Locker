30a160129cff9d913804789ff32627b442777c2a
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-24 02:59:43 +0300

    adding user interaction

--- run.py
+++ run.py
@@ -59,9 +59,44 @@ def display_new_creds():
 
     '''
     return Creds.display_accounts()
+def create_password():
+    
 #main function 
 def main():
- print("Holla there,Cómo estás,")
+    print("Holla there,Cómo estás,Welcome to your Password Locker App. What is your name?")
+    user_name = input()
+    print(f"Hello {user_name}. what would you like to do?")
+    print('\n')
+    while True:
+                    print("Use these short codes : na - create a new account, da - display contacts,lg-login into account, fa -find an account, ex -exit the password locker list ")
 
+                    short_code = input().lower()
+
+                    if short_code == 'na':
+                            print("New Account")
+                            print("-"*10)
+
+                            print ("Please Enter your First name ....")
+                            f_name = input()
+
+                            print("Please Enter your Last name ...")
+                            l_name = input()
+
+                            print("Please Enter your Phone number ...")
+                            p_number = input()
+
+                            print("Please Enter your Email address ...")
+                            e_address = input()
+
+                            print("Please Enter your Username ...")
+                            u_name = input()
+
+                            print("Please Enter your Password ...")
+                            p_word = input()
+                       
+
+
+if __name__ ==  '__main__':
+    main()
 
 
