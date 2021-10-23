d2949819784eb674e1c4eedd6fcfdfa993b2fe8d
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-24 01:26:55 +0300

    method to create new user

--- run.py
+++ run.py
@@ -1,3 +1,9 @@
 #!/usr/bin/env python3.8
 from user import User
 from credentials import Creds
+def new_user(first_name,last_name,phone_number,email,username,password):
+    '''
+    new_user method creates a new user
+    '''
+    new_user = User(first_name,last_name,phone_number,email,username,password)
+    return new_user
