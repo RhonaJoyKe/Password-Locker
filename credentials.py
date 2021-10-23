692b27ac1b8f598aff1f83cffe2d174ca42c7132
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-23 23:20:51 +0300

    adds the arguments of the creds class

--- credentials.py
+++ credentials.py
@@ -4,3 +4,8 @@ class Creds:
     instances of the different social  accounts
     '''
 user_accounts=[]
+def __init__ (self,account_name,account_username,account_password):
+        self.account_name = account_name
+        self.account_username = account_username
+        self.account_password = account_password
+       
