a32d85053b6be8b967821ccdc3bffadadcfcdec3
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-23 20:31:16 +0300

    adds the class User and instantiates class arguments

---
+++ user.py
@@ -0,0 +1,12 @@
+class User:
+    """
+    Class that generates new 
+    instances of user accounts.
+    """
+user_list = [] 
+def __init__(self,first_name,last_name,phone_number,email,password):
+        self.first_name = first_name
+        self.last_name = last_name
+        self.phone_number = phone_number
+        self.email = email
+        self.password= password
