490109f41cb9fce940583b79d09c9e635f716b81
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-23 20:40:14 +0300

    adds the save method to the user class

--- user.py
+++ user.py
@@ -10,3 +10,11 @@ def __init__(self,first_name,last_name,phone_number,email,password):
         self.phone_number = phone_number
         self.email = email
         self.password= password
+
+def save_user(self):
+
+        '''
+        save_user method saves contact objects into user_list
+        '''
+
+        User.user_list.append(self)
