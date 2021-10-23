169746b06ac2e1284e90f6eb5b99b944f838df47
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-23 22:17:46 +0300

    adds the delete function and text case

--- user.py
+++ user.py
@@ -14,7 +14,13 @@ class User:
     def save_user(self):
 
         '''
-        save_user method saves contact objects into user_list
+        save_user method saves user objects into user_list
         '''
 
         User.user_list.append(self)
+    def delete_user(self):
+        '''
+        delete_user method deletes user objects from user_list
+
+        '''
+        User.user_list.remove(self)
--- user_test.py
+++ user_test.py
@@ -23,17 +23,34 @@ class TestUser(unittest.TestCase):
         '''
         Set up method to run before each test cases.
         '''
-        self.new_user = User("Rhona","Koome","0706555078","rhonajoy8@gmail.com","12345") # create contact object
+        self.new_user = User("Rhona","Koome","0706555078","rhonajoy8@gmail.com","12345") # create user object
 
     
-    
+    def tearDown(self):
+            '''
+            tearDown method that does clean up after each test case has run.
+            '''
+            User.user_list = []
+
+
     def test_save_user(self):
         '''
-        test_save_contact test case to test if the contact object is saved into
-         the contact list
+        test_save_contact test case to test if the user object is saved into
+         the user list
         '''
         self.new_user.save_user()
         self.assertEqual(len(User.user_list),1)
+    
+    def test_delete_user(self):
+            '''
+            test_delete_contact to test if we can remove a contact from our contact list
+            '''
+            self.new_user.save_user()
+            test_contact = User("Test","user","0712345678","test@user.com","trhj89") # new contact
+            test_contact.save_user()
+
+            self.new_user.delete_user()# Deleting a contact object
+            self.assertEqual(len(User.user_list),1) 
 
 if __name__ ==  '__main__':
     unittest.main()
