7e7c277355e6b6a3074b6b6c3eaf93d9fcd0899b
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-24 01:50:19 +0300

    adds the new creds method in run.py

--- run.py
+++ run.py
@@ -2,6 +2,8 @@
 from user import User
 from credentials import Creds
 
+## for the user class
+
 def new_user(first_name,last_name,phone_number,email,username,password):
     '''
     new_user method creates a new user
@@ -19,5 +21,20 @@ def delete_new_user(user):
     '''
     deletes_user
     '''
-    user.delete_user
+    user.delete_user()
+def find_new_user(user):
+    '''
+    finds users
+    '''
+    user.find_by_username()
+##credentials class
+def new_creds(account_name,account_username,account_password):
+    '''
+    creates new account in credentials
+
+    '''
+    new_creds= Creds(account_name,account_username,account_password)
+    return new_creds
+
+
 
