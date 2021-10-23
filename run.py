e3bed72f51916395b437e7c7905715b729c777cc
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-24 01:35:28 +0300

    creates functions to add and delete new users

--- run.py
+++ run.py
@@ -1,9 +1,23 @@
 #!/usr/bin/env python3.8
 from user import User
 from credentials import Creds
+
 def new_user(first_name,last_name,phone_number,email,username,password):
     '''
     new_user method creates a new user
     '''
     new_user = User(first_name,last_name,phone_number,email,username,password)
     return new_user
+
+def save_new_user(user):  
+    '''
+    save_user
+    '''
+    user.save_user()
+
+def delete_new_user(user):  
+    '''
+    deletes_user
+    '''
+    user.delete_user
+
