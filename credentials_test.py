f8c76ac6529a4e1736d4bc4b8ed19f69f5e51ee9
Moringa School <rhonajoy.koome@student.moringaschool.com>
2021-10-24 00:18:07 +0300

    adds the Setup and initialize methods

--- credentials_test.py
+++ credentials_test.py
@@ -4,3 +4,13 @@ class TestCreds(unittest.TestCase):
     '''
     Test class that defines test cases for the credentials class behaviours
     '''
+    def setUp(self):
+        '''
+        set up method to run before each test cases
+        '''
+        self.new_cred= Creds("Twitter","Roro","46677")
+    
+    def test_init(self):
+        '''
+        tests if the object is initialized properlyg
+        '''
